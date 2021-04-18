9f1b397287244cfe74ccbba18ca70bf0964189c5
SANKETJAIN1111 <sigmasanket2712@gmail.com>
2021-04-18 19:07:40 +0530

    first commit

---
+++ src/main/java/com/learner/RestapispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.learner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapispringbootApplication.class, args);
+	}
+
+}
